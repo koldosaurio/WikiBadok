c972f3be83c0139671fddfe5a1b39dd2842b500c
koldosaurio <37398814+koldosaurio@users.noreply.github.com>
2023-03-26 15:58:12 +0200

    edozein taldeko diskak eskuratzen ditu ondo sartzen bada talde izena

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import proba
 
-print(proba.izenak_lortu())
+print(proba.taldearen_diskoak_lortu('oskorri'))
--- proba.py
+++ proba.py
@@ -8,9 +8,18 @@ def izenak_lortu():
 	izenLista=[]
 	
 	soup =  BeautifulSoup(html, 'html.parser')
-	sections = soup.body('section')
-	for section in sections:
+	for section in soup.body('section'):
 		if section['class'][0] == 'letra_bakoitza':
 			for a in section('a'):
 				izenLista.append(a.string)
 	return izenLista
+
+def taldearen_diskoak_lortu(taldeIzena):
+	url = "https://www.badok.eus/euskal-musika/"+taldeIzena
+	html = urlopen(url).read().decode("utf-8")
+	soup =  BeautifulSoup(html, 'html.parser')
+	diskak = []
+	for div in soup.body.find(id='diskografia').find_all(attrs={'class': 'row'}):
+		if 'konpartitu' not in div.attrs['class']:
+			diskak.append(div.find_all(attrs={'class':'col-md-4'})[1].a.string)
+	return diskak
