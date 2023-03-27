380624ed068607b40c120c569a4f739baa90d8de
loreaflower <lorealoinaz03@gmail.com>
2023-03-27 16:58:40 +0200

    kodearen estruktura aldatuta datuen lorketa errazteko

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import proba
 
 #print(proba.izenak_lortu())
-datuak=proba.izenak_lortu()
-# web orrian topatzen dituen izen guztiei diskak aldagaia gehituko die
-for i in range(len(datuak)):
-    datuak[i]['diskak'] = proba.taldearen_diskoak_lortu(datuak[i]['url'].replace(' ','%20'))
-print(datuak)
+print(proba.datuak_lortu())
 
--- proba.py
+++ proba.py
@@ -14,11 +14,30 @@ def izenak_lortu():
 				izenLista.append({'izena': a.string, 'url': a['href']})
 	return izenLista
 
-def taldearen_diskoak_lortu(url):
-	html = urlopen(url).read().decode("utf-8")
-	soup =  BeautifulSoup(html, 'html.parser')
+def taldearen_diskoak_lortu(soup):
 	diskak = []
 	for div in soup.body.find(id='diskografia').find_all(attrs={'class': 'row'}):
 		if 'konpartitu' not in div.attrs['class']:
 			diskak.append(div.find_all(attrs={'class':'col-md-4'})[1].a.string)
 	return diskak
+
+def taldearen_biografia_lortu(soup):
+    biografia=''
+    
+    return biografia
+    
+
+def taldearen_informazioa_lortu(url, datuak):
+    html = urlopen(url).read().decode("utf-8")
+    soup =  BeautifulSoup(html, 'html.parser')
+    datuak['diskak']=taldearen_diskoak_lortu(soup)
+    datuak['biografia']=taldearen_biografia_lortu(soup)
+    
+    return datuak
+
+def datuak_lortu():
+    datuak=izenak_lortu()
+    for i in range(10):
+        datuak[i] =taldearen_informazioa_lortu(datuak[i]['url'].replace(' ','%20'), datuak[i])
+    return datuak
+        
