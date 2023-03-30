47c19ff0abfca6de8484dfa0c58beeda786f024c
loreaflower <lorealoinaz03@gmail.com>
2023-03-30 16:59:16 +0200

    kodeak gehituta

--- aldagaiGlobalak.py
+++ aldagaiGlobalak.py
@@ -5,3 +5,10 @@ Created on Thu Mar 30 16:37:53 2023
 @author: Koldo
 """
 ZENBAT_IZEN=50
+
+KODEAK={
+'honako hau da':'P31',
+'album':'Q482994',
+'interpretatzailea':'P175',
+'diskografia':'P358'
+}
