f5d536815acd43320016f9eb5aec327429e5c3c4
koldosaurio <37398814+koldosaurio@users.noreply.github.com>
2023-04-06 15:23:00 +0200

    maina ahaztu jata barkatuuu

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import aldagaiGlobalak as ag
 
 
 datuak=funtzioak.datuak_lortu()
+print('\nDatuak lortuta\n')
 #print(proba.izenak_lortu())
 with open('taldeak.csv', 'w', newline='') as file:
 	writer = csv.writer(file)
-	for i in range(ag.ZENBAT_IZEN):
-		for diska in datuak[i]['diskak']:
-			writer.writerow([datuak[i]['izena'], diska['izena'], datuak[i]['url']])
+	for i in range(len(datuak)):
+		try:
+			for diska in datuak[i]['diskak']:
+				writer.writerow([datuak[i]['izena'], diska['izena'], datuak[i]['url']])
+		except:
+			print(datuak[i]['izena'] + ' taldearen diskak: \n' + datuak[i]['diskak'])
 #print(funtzioak.datuak_lortu())
