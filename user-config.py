a8fc53b11deb43aa6187762dfee453d319fb8b97
loreaflower <lorealoinaz03@gmail.com>
2023-04-11 16:32:08 +0200

    userconfig.py gehitu diot pywikibot instalatzen duzuenean erabiltzeko

---
+++ user-config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# This is a sample file. You should use generate_user_files.py 
+# to create your user-config.py file.
+
+
+mylang = 'test'
+family = 'wikidata'
+usernames['wikidata']['test'] = 'Anekoldolorea'
+
+
+#mylang = 'eu'
+#family= 'wikipedia'
+#usernames['wikipedia']['eu'] = 'Anekoldolorea'
+
+
+#mylang = 'wikidata'
+#family = 'wikidata'
+#usernames['wikidata']['wikidata'] = 'Anekoldolorea'
+password_file = None
