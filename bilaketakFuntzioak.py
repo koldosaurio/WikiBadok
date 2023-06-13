da9b3d3138c57a4032ae934b86bf6b8e95d3f90f
LAPTOP-G3O8S6AU\anepa <anepaniagua0@gmail.com>
2023-06-13 16:11:14 +0200

    gauzak aldauta baño probau gabe

--- bilaketakFuntzioak.py
+++ bilaketakFuntzioak.py
@@ -9,7 +9,6 @@ import traceback
 
 # funtzioa
 def wikidatanDago(taldeIzena):
-    emaitza = []
     try:
         # Wikidataren orria jaso
         wikidata_site = pywikibot.Site('wikidata', 'wikidata')
@@ -19,28 +18,32 @@ def wikidatanDago(taldeIzena):
         search_results = wikidata_site.search_entities(taldeIzena, language='eu')
         
         # print the search results
-        emaitza = []
-        emaitzak = []
-        if(search_results != None): # Orriren bat badago
-            for result in search_results:
-                item = pywikibot.ItemPage(wikidata_site, result['id']) 
-                item.get()
-                try:
-                    label = item.labels['eu'] # Euskerazko atalean badu izenbururik?
-                except KeyError:
-                    label = 'KeyError'
+        # emaitza = []
+        # emaitzak = []
+        if(search_results == None):
+            return False;
+        else:
+            return True;
+        # if(search_results != None): # Orriren bat badago
+        #     for result in search_results:
+        #         item = pywikibot.ItemPage(wikidata_site, result['id']) 
+        #         item.get()
+        #         try:
+        #             label = item.labels['eu'] # Euskerazko atalean badu izenbururik?
+        #         except KeyError:
+        #             label = 'KeyError'
                 
-                try:
-                    description = item.descriptions['eu'] # Euskerazko atalean badu deskribapenik?
-                except:
-                    description = 'KeyError'
-                emaitza = [item.id, label, description] # Orri hau
-                emaitzak.append(emaitza) # Taldeak dituen orri guztien bektorea
-        else: # Ez badago orririk
-            emaitza = ['EzDago', taldeIzena, '']
+        #         try:
+        #             description = item.descriptions['eu'] # Euskerazko atalean badu deskribapenik?
+        #         except:
+        #             description = 'KeyError'
+        #         emaitza = [item.id, label, description] # Orri hau
+        #         emaitzak.append(emaitza) # Taldeak dituen orri guztien bektorea
+        # else: # Ez badago orririk
+        #     emaitza = ['EzDago', taldeIzena, '']
     except Exception:
         traceback.print_exc()
-    return emaitzak
+    return False;
 
 
             
--- bilaketakProbak.py
+++ bilaketakProbak.py
@@ -4,44 +4,51 @@ Created on Mon May  8 15:50:12 2023
 
 @author: anepa
 """
+import csvreader
 import bilaketakFuntzioak
-import csv
+# import csv
 
-taldeak=[]
+a=open('./taldeak.csv', 'w', newline='')
+b= open('./diskak.csv', 'w', newline='')
+c= open('./kantak.csv', 'w', newline='')
 
-# Taldeen CSVa ireki
-with open('D:/SourceTree/WikiBadok/taldeak.csv', 'r') as file:
-    csvreader = csv.reader(file)
-    # Errenkada bakoitzeko izena taldeak[] bektorean sartu
-    for row in csvreader:
-        taldeIzena = row[1]
-        taldeak.append(taldeIzena)
-        # Ongi sartuta dauden taldeak
-        with open('./ondo.csv', 'w', newline = '') as ondo:
-            writerOndo = csv.writer(ondo)
-            # Ez dauden taldeak
-            with open('./ezDago.csv', 'w', newline = '') as ezDago:
-                writerEzDago = csv.writer(ezDago)
-                # Musikariak ez diren taldeak
-                with open('./arraro.csv', 'w', newline = '') as arraro:
-                    writerArraro = csv.writer(arraro)
-                    for t in taldeak:
-                        emaitzak = bilaketakFuntzioak.wikidatanDago(t) #Funtzioari deitu
-                        for taldeOrriak in emaitzak : #[orria1, orria2, ...]
-                            for orria in taldeOrriak: #[ID, label, description]
-                                # 1. ez dago ezer
-                                if(orria[0] == 'EzDago'): #Ez du ID
-                                    writerEzDago.writerow(orria)                    
-                                # 2. KEYERROR 
-                                elif orria[1] == 'KeyError' or orria[2] == 'KeyError':
-                                    # KEYERROR AZTERTZEKO FUNTZIOA
-                                    print("KEYERROR")
-                                # 3. musikaria da?
-                                elif 'musikari' in orria[2] or 'kantari' in orria[2] or 'abeslari' in orria[2] or 'talde' in orria[2]:
-                                    writerOndo.writerow(orria)
-                                else:
-                                    writerArraro.writerow(orria) # Beste guztiak
-                arraro.close()
-            ezDago.close()
-        ondo.close()
-    file.close()
+datuak = csvreader.lortu_datuak(a,b,c);
+for taldea in datuak:
+    emaitza = bilaketakFuntzioak.wikidatanDago(taldea);
+    print(taldea + ": " + emaitza);
+# # Taldeen CSVa ireki
+# with open('D:/SourceTree/WikiBadok/taldeak.csv', 'r') as file:
+#     csvreader = csv.reader(file)
+#     # Errenkada bakoitzeko izena taldeak[] bektorean sartu
+#     for row in csvreader:
+#         taldeIzena = row[1]
+#         taldeak.append(taldeIzena)
+#         # Ongi sartuta dauden taldeak
+#         with open('./ondo.csv', 'w', newline = '') as ondo:
+#             writerOndo = csv.writer(ondo)
+#             # Ez dauden taldeak
+#             with open('./ezDago.csv', 'w', newline = '') as ezDago:
+#                 writerEzDago = csv.writer(ezDago)
+#                 # Musikariak ez diren taldeak
+#                 with open('./arraro.csv', 'w', newline = '') as arraro:
+#                     writerArraro = csv.writer(arraro)
+#                     for t in taldeak:
+#                         emaitzak = bilaketakFuntzioak.wikidatanDago(t) #Funtzioari deitu
+#                         for taldeOrriak in emaitzak : #[orria1, orria2, ...]
+#                             for orria in taldeOrriak: #[ID, label, description]
+#                                 # 1. ez dago ezer
+#                                 if(orria[0] == 'EzDago'): #Ez du ID
+#                                     writerEzDago.writerow(orria)                    
+#                                 # 2. KEYERROR 
+#                                 elif orria[1] == 'KeyError' or orria[2] == 'KeyError':
+#                                     # KEYERROR AZTERTZEKO FUNTZIOA
+#                                     print("KEYERROR")
+#                                 # 3. musikaria da?
+#                                 elif 'musikari' in orria[2] or 'kantari' in orria[2] or 'abeslari' in orria[2] or 'talde' in orria[2]:
+#                                     writerOndo.writerow(orria)
+#                                 else:
+#                                     writerArraro.writerow(orria) # Beste guztiak
+#                 arraro.close()
+#             ezDago.close()
+#         ondo.close()
+#     file.close()
